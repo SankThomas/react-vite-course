81110af56fd6c1ae26924ab50061dc442a6c3421
SankThomas <thomassankara74@gmail.com>
2025-12-15 16:54:42 +0300

    render section components

---
+++ 3-portfolio/src/App.jsx
@@ -0,0 +1,29 @@
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Skills from "./components/Skills";
+import Projects from "./components/Projects";
+import Testimonials from "./components/Testimonials";
+import Blog from "./components/Blog";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+
+export default function App() {
+  return (
+    <div>
+      <Header />
+
+      <main>
+        <Hero />
+        <About />
+        <Skills />
+        <Projects />
+        <Testimonials />
+        <Blog />
+        <Contact />
+      </main>
+
+      <Footer />
+    </div>
+  );
+}
